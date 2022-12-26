0721382a4f3c47db4c57084d05e5eb2401e6c342
salli <sezininyayla@gmail.com>
2022-12-26 23:57:35 +0300

    updated

--- UI/Controllers/HomeController.cs
+++ UI/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
@@ -8,6 +9,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using UI.Models;
+using UI.Services;
 using UI.Services.IServices;
 
 namespace UI.Controllers
@@ -43,6 +45,23 @@ namespace UI.Controllers
                 model = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
             }
             return View(model);
+        }
+
+
+        [HttpPost]
+        [ActionName("Details")]
+        public async Task<IActionResult> DetailsPost(ProductDto productDto)
+        {
+            CartDto cartDto = new()
+            {
+                CartHeader = new CartHeaderDto
+                {
+                    UserId=User.Claims.Where(u=>u.Type=="sub")?.FirstOrDefault()?.Value
+                }
+
+            };
+            CartDeatailsDto cartDeatailsDto = new() { };
+            return View();
         }
 
         public IActionResult Privacy()
